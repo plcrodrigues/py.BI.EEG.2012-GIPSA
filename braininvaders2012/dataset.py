a0e0617ce9d714850ed0bdceda022b68ea7c44c1
Pedro Coelho <pedro-luiz.coelho-rodrigues@gipsa-lab.fr>
2019-04-29 11:06:45 +0200

    first commit

---
+++ braininvaders2012/dataset.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import mne
+import numpy as np
+from braininvaders2012 import download as dl
+import os
+import glob
+import zipfile
+import yaml
+from scipy.io import loadmat
+
+BI2012a_URL = 'https://zenodo.org/record/2649069/files/'
+
+class BrainInvaders2012():
+    '''
+
+    '''
+
+    def __init__(self, Training=True, Online=False):
+
+        self.training = Training
+        self.online = Online
+        self.subject_list = list(range(1, 25 + 1))
+
+    def _get_single_subject_data(self, subject):
+        """return data for a single subject"""
+
+        file_path_list = self.data_path(subject)
+        sessions = {}
+        for file_path in file_path_list:
+
+            session_name = 'session_1'
+            condition = file_path.split('_')[-1].split('.')[0].split(os.sep)[-1]
+            run_name = 'run_' + condition
+
+            chnames = ['F7',
+                       'F3',
+                       'Fz',
+                       'F4',
+                       'F8',
+                       'T7',
+                       'C3',
+                       'Cz',
+                       'C4',
+                       'T8',
+                       'P7',
+                       'P3',
+                       'Pz',
+                       'P4',
+                       'P8',
+                       'O1',
+                       'O2',
+                       'STI 014']
+            chtypes = ['eeg'] * 17 + ['stim']               
+
+            X = loadmat(file_path)[condition].T
+            S = X[1:18,:]
+            stim = (X[18,:] + X[19,:])[None,:]
+            X = np.concatenate([S, stim])
+
+            info = mne.create_info(ch_names=chnames, sfreq=512,
+                                   ch_types=chtypes, montage='standard_1020',
+                                   verbose=False)
+            raw = mne.io.RawArray(data=X, info=info, verbose=False)
+
+            sessions[session_name] = {}
+            sessions[session_name][run_name] = raw
+
+        return sessions
+
+    def data_path(self, subject, path=None, force_update=False,
+                  update_path=None, verbose=None):
+
+        if subject not in self.subject_list:
+            raise(ValueError("Invalid subject number"))
+
+        # check if has the .zip
+        url = BI2012a_URL + 'subject_' + str(subject).zfill(2) + '.zip'
+        path_zip = dl.data_path(url, 'BRAININVADERS2012')
+        path_folder = path_zip.strip('subject_' + str(subject).zfill(2) + '.zip')
+
+        # check if has to unzip
+        if not(os.path.isdir(path_folder + 'subject{:d}/'.format(subject))):
+            print('unzip', path_zip)
+            zip_ref = zipfile.ZipFile(path_zip, "r")
+            zip_ref.extractall(path_folder)
+
+        subject_paths = []
+
+        # filter the data regarding the experimental conditions
+        if self.training:
+            subject_paths.append(path_folder + 'subject_' + str(subject).zfill(2) + '/training.mat')
+        if self.online:
+            subject_paths.append(path_folder + 'subject_' + str(subject).zfill(2) + '/online.mat')
+
+        return subject_paths
---
+++ example_classification.py
@@ -0,0 +1,60 @@
+
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from pyriemann.classification import MDM
+from pyriemann.estimation import XdawnCovariances
+from braininvaders2013.dataset import BrainInvaders2013
+import numpy as np
+import mne
+"""
+=============================
+Classification of the trials
+=============================
+
+This example shows how to extract the epochs from the dataset of a given
+subject and then classify them using Machine Learning techniques using
+Riemannian Geometry. 
+
+"""
+# Authors: Pedro Rodrigues <pedro.rodrigues01@gmail.com>
+#
+# License: BSD (3-clause)
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# define the dataset instance
+dataset = BrainInvaders2013(NonAdaptive=True, Adaptive=False, Training=True, Online=False)
+
+# get the data from subject of interest
+subject = dataset.subject_list[0]
+data = dataset._get_single_subject_data(subject)
+session = 1
+raw = data['session_' + str(session)]['run_3']
+
+# filter data and resample
+fmin = 1
+fmax = 24
+raw.filter(fmin, fmax, verbose=False)
+raw.resample(sfreq=128, verbose=False)
+
+# detect the events and cut the signal into epochs
+events = mne.find_events(raw=raw, shortest_event=1, verbose=False)
+event_id = {'NonTarget': 33286, 'Target': 33285}
+epochs = mne.Epochs(raw, events, event_id, tmin=0.0, tmax=1.0, baseline=None, verbose=False)
+
+# get trials and labels
+X = epochs.get_data()
+y = events[:, -1]
+y[y == 33286] = 0
+y[y == 33285] = 1
+
+# cross validation
+skf = StratifiedKFold(n_splits=5)
+clf = make_pipeline(XdawnCovariances(estimator='lwf', classes=[1]), MDM())
+scr = cross_val_score(clf, X, y, cv=skf, scoring='roc_auc')
+
+# print results of classification
+print('subject', subject, 'session', session)
+print('mean AUC :', scr.mean())
+
