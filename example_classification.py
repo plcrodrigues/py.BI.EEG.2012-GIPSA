bf9c28647aa2cb39f6b7b5987e5107ed0ad58931
Pedro Coelho <pedro-luiz.coelho-rodrigues@gipsa-lab.fr>
2019-05-06 14:47:28 +0200

    ...

--- example_classification.py
+++ example_classification.py
@@ -1,9 +1,11 @@
 
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.preprocessing import LabelEncoder
 from pyriemann.classification import MDM
-from pyriemann.estimation import XdawnCovariances
-from braininvaders2013.dataset import BrainInvaders2013
+from pyriemann.estimation import XdawnCovariances, ERPCovariances
+from braininvaders2012.dataset import BrainInvaders2012
+from tqdm import tqdm
 import numpy as np
 import mne
 """
@@ -24,37 +26,37 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # define the dataset instance
-dataset = BrainInvaders2013(NonAdaptive=True, Adaptive=False, Training=True, Online=False)
+dataset = BrainInvaders2012(Training=True)
 
+scr = {}
 # get the data from subject of interest
-subject = dataset.subject_list[0]
-data = dataset._get_single_subject_data(subject)
-session = 1
-raw = data['session_' + str(session)]['run_3']
+for subject in dataset.subject_list:
 
-# filter data and resample
-fmin = 1
-fmax = 24
-raw.filter(fmin, fmax, verbose=False)
-raw.resample(sfreq=128, verbose=False)
+	data = dataset._get_single_subject_data(subject)
+	session = 1
+	raw = data['session_' + str(session)]['run_training']
 
-# detect the events and cut the signal into epochs
-events = mne.find_events(raw=raw, shortest_event=1, verbose=False)
-event_id = {'NonTarget': 33286, 'Target': 33285}
-epochs = mne.Epochs(raw, events, event_id, tmin=0.0, tmax=1.0, baseline=None, verbose=False)
+	# filter data and resample
+	fmin = 1
+	fmax = 24
+	raw.filter(fmin, fmax, verbose=False)
+	raw.resample(sfreq=128, verbose=False)
 
-# get trials and labels
-X = epochs.get_data()
-y = events[:, -1]
-y[y == 33286] = 0
-y[y == 33285] = 1
+	# detect the events and cut the signal into epochs
+	events = mne.find_events(raw=raw, shortest_event=1, verbose=False)
+	event_id = {'NonTarget': 1, 'Target': 2}
+	epochs = mne.Epochs(raw, events, event_id, tmin=0.0, tmax=0.6, baseline=None, verbose=False)
 
-# cross validation
-skf = StratifiedKFold(n_splits=5)
-clf = make_pipeline(XdawnCovariances(estimator='lwf', classes=[1]), MDM())
-scr = cross_val_score(clf, X, y, cv=skf, scoring='roc_auc')
+	# get trials and labels
+	X = epochs.get_data()
+	y = events[:, -1]
 
-# print results of classification
-print('subject', subject, 'session', session)
-print('mean AUC :', scr.mean())
+	# cross validation
+	skf = StratifiedKFold(n_splits=5)
+	clf = make_pipeline(ERPCovariances(estimator='lwf', classes=[1]), MDM())
+	scr[subject] = cross_val_score(clf, X, LabelEncoder().fit_transform(y), cv=skf, scoring='roc_auc').mean()
+
+	# print results of classification
+	print('subject', subject, 'session', session)
+	print('mean AUC :', scr[subject])
 
